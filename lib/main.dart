78d83f7c0087eb1e07df8becec6065ac0484d9cb
Jason <jmrunkle@gmail.com>
2020-04-30 17:57:17 -0700

    Final version of xylophone app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,8 +9,32 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              nextBar(Colors.red, 1),
+              nextBar(Colors.orange, 2),
+              nextBar(Colors.yellow, 3),
+              nextBar(Colors.green, 4),
+              nextBar(Colors.blue, 5),
+              nextBar(Colors.indigo, 6),
+              nextBar(Colors.deepPurple, 7),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget nextBar(Color color, int note) {
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.all(0.0),
+        child: Container(
+          color: color,
+          width: double.infinity,
+        ),
+        onPressed: () =>
+            AssetsAudioPlayer.playAndForget(Audio('assets/note$note.wav')),
       ),
     );
   }
